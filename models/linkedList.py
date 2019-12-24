91b2b68e37904ffc47f6fb30b25929b851879756
Zach Wahrer <zachtheclimber@gmail.com>
2019-12-24 12:36:11 -0700

    Create add() to generate node at end of list

--- models/linkedList.py
+++ models/linkedList.py
@@ -1,7 +1,24 @@
 # Linked List
 
+import unittest
+
+
 class ListNode:
 
     def __init__(self, val):
         self.val = val
         self.next = None
+
+    def add(self, new_val):
+        if self.next is None:
+            self.next = ListNode(new_val)
+        else:
+            self.next.add(new_val)
+        return self
+
+    def add_itter(self, new_val):
+        """Reduntant add function"""
+        current_node = self
+        while current_node.next:
+            current_node = self.next
+        current.next = ListNode(new_val)
