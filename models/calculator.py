558f36063f86a6b26b4e454f1f3b588761edcb7d
Zach Wahrer <zachtheclimber@gmail.com>
2019-12-24 16:15:18 -0700

    Add subtract(), multiply(), sum() and tests

--- models/calculator.py
+++ models/calculator.py
@@ -6,21 +6,19 @@ class Calculator:
         return x + y
 
     def subtract(self, x, y):
-        pass
+        return x - y
 
     def multiply(self, x, y):
-        pass
+        return x * y
 
     def sum(self, numbers):  # Take in an array of nums
-        pass
+        sum = 0
+        for i in numbers:
+            sum += i
+        return sum
 
     def max(self, numbers):  # Find max of list of nums
         pass
-
-
-my_calculator = Calculator()
-
-print(my_calculator.add(2, 7))
 
 
 class TestCalculator(unittest.TestCase):
@@ -42,6 +40,45 @@ class TestCalculator(unittest.TestCase):
         y = 5
         ans = 10
         self.assertEqual(Calculator().add(x, y), ans)
+
+    def test_subtract_five_five(self):
+        x = 5
+        y = 5
+        ans = 0
+        self.assertEqual(Calculator().subtract(x, y), ans)
+
+    def test_subtract_8_10(self):
+        x = 8
+        y = 10
+        ans = -2
+        self.assertEqual(Calculator().subtract(x, y), ans)
+
+    def test_multiply_five_five(self):
+        x = 5
+        y = 5
+        ans = 25
+        self.assertEqual(Calculator().multiply(x, y), ans)
+
+    def test_multiply_ten_zero(self):
+        x = 10
+        y = 0
+        ans = 0
+        self.assertEqual(Calculator().multiply(x, y), ans)
+
+    def test_sum_minus_11519(self):
+        nums = [-1, -1, -5, -1, -9]
+        ans = -17
+        self.assertEqual(Calculator().sum(nums), ans)
+
+    def test_sum_8892310(self):
+        nums = [8, 8, 9, 2, 3, 1, 0]
+        ans = 31
+        self.assertEqual(Calculator().sum(nums), ans)
+
+    def test_sum_mixed_3149(self):
+        nums = [3, 1, 4, -9]
+        ans = -1
+        self.assertEqual(Calculator().sum(nums), ans)
 
 
 if __name__ == "__main__":
