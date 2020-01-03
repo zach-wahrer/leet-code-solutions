32632831f5c3a185309b0c78ab027feb3f8c59cf
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-03 09:38:47 -0700

    Improve tests to support multiple solutions

--- 1304-Find-N-Unique-Int-Sum-Zero.py
+++ 1304-Find-N-Unique-Int-Sum-Zero.py
@@ -7,33 +7,24 @@ import unittest
 
 
 def unique_ints(numbers: int) -> list:
-    return 1
+    return [1, 1]
 
 
 class TestUniqueList(unittest.TestCase):
 
     def test_5(self):
-        input = unique_ints(5)
-        possibles = [
-            [-7, -1, 1, 3, 4],
-            [-5, -1, 1, 2, 3],
-            [-3, -1, 2, -2, 4]
-        ]
-        for output in possibles:
-            if input == output:
-                self.assertEqual(input, output)
-                break
-            if output == possibles[(len(possibles) - 1)]:
-                self.assertEqual(input, possibles[(len(possibles) - 1)])
+        input = sum(unique_ints(5))
+        output = 0
+        self.assertEqual(input, output)
 
     def test_3(self):
-        input = unique_ints(3)
-        output = [-1, 0, 1]
+        input = sum(unique_ints(3))
+        output = 0
         self.assertEqual(input, output)
 
     def test_1(self):
-        input = unique_ints(1)
-        output = [0]
+        input = sum(unique_ints(1))
+        output = 0
         self.assertEqual(input, output)
 
 
