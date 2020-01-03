636cd5952d0001d6652ac3b3ceddd9b3c01be8b7
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-03 10:13:18 -0700

    Simple solution (faster than 58% other Py3)

--- 1304-Find-N-Unique-Int-Sum-Zero.py
+++ 1304-Find-N-Unique-Int-Sum-Zero.py
@@ -1,13 +1,16 @@
 '''
 Psudocode:
-
+    Make a list from 1:n
+    Replace list[0] with the sum of remaining list
 '''
 
 import unittest
 
 
-def unique_ints(numbers: int) -> list:
-    return [1, 1]
+def unique_ints(number: int) -> list:
+    numbers_list = list(range(number))
+    numbers_list[0] = sum(numbers_list[1:]) * -1
+    return numbers_list
 
 
 class TestUniqueList(unittest.TestCase):
@@ -24,6 +27,16 @@ class TestUniqueList(unittest.TestCase):
 
     def test_1(self):
         input = sum(unique_ints(1))
+        output = 0
+        self.assertEqual(input, output)
+
+    def test_47(self):
+        input = sum(unique_ints(47))
+        output = 0
+        self.assertEqual(input, output)
+
+    def test_1000(self):
+        input = sum(unique_ints(1000))
         output = 0
         self.assertEqual(input, output)
 
