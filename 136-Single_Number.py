989b109b0f4a3d4ec32df6264d6416ea1c730a44
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-03 10:35:55 -0700

    Add initial tests

---
+++ 136-Single_Number.py
@@ -0,0 +1,39 @@
+'''
+Psudocode:
+    Go through input numbers
+        Add number to unique list if it isn't there
+        Remove if it is
+'''
+
+import unittest
+
+
+def find_single_int(nums: list) -> int:
+    pass
+
+
+class TestSingleInt(unittest.TestCase):
+
+    def test_221(self):
+        input = find_single_int([2, 2, 1])
+        output = 1
+        self.assertEqual(input, output)
+
+    def test_41212(self):
+        input = find_single_int([4, 1, 2, 1, 2])
+        output = 4
+        self.assertEqual(input, output)
+
+    def test_81518122(self):
+        input = find_single_int([8, 1, 5, 1, 8, 1, 2, 2])
+        output = 5
+        self.assertEqual(input, output)
+
+    def test_448716181227(self):
+        input = find_single_int([4, 4, 8, 7, 1, 6, 1, 8, 1, 2, 2, 7])
+        output = 6
+        self.assertEqual(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
