129cc96bc4d5f3f72b3192260cfa9b0cf62b5e47
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-04 09:32:39 -0700

    Add initial tests

---
+++ 9-Palindrome-Number.py
@@ -0,0 +1,38 @@
+'''
+Psudocode:
+
+'''
+
+import unittest
+
+
+def check_palindrome(x: int) -> bool:
+    pass
+
+
+class TestPalindromeCheck(unittest.TestCase):
+
+    def test_121(self):
+        self.assertTrue(check_palindrome(121))
+
+    def test_minus121(self):
+        self.assertFalse(check_palindrome(-121))
+
+    def test_10(self):
+        self.assertFalse(check_palindrome(10))
+
+    def test_0(self):
+        self.assertTrue(check_palindrome(0))
+
+    def test_900009(self):
+        self.assertTrue(check_palindrome(900009))
+
+    def test_1234321(self):
+        self.assertTrue(check_palindrome(1234321))
+
+    def test_891224(self):
+        self.assertFalse(check_palindrome(891224))
+
+
+if __name__ == '__main__':
+    unittest.main()
