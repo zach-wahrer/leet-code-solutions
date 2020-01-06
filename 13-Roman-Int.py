0a5688baaa87554f42f8d579cf7ec07fad589aaa
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-06 12:45:37 -0700

    Add O(n) solution
    Faster than 95% of Python3 submissions.

--- 13-Roman-Int.py
+++ 13-Roman-Int.py
@@ -1,13 +1,28 @@
 '''
 Psudocode:
-
+    Create dict for roman: int values
+    Iterate over string, summing number
+    If char is I, X, or C, look ahead to see if next letter is valued less
+        If true, subtract I, X, or C from it
 '''
 
 import unittest
 
 
 def convert_roman(s: str) -> int:
-    pass
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    out_number = 0
+    s_len = len(s)
+    for posistion, char in enumerate(s):
+        char_value = values[char]
+        if posistion + 1 < s_len:
+            if char_value < values[s[posistion + 1]]:
+                out_number -= char_value
+            else:
+                out_number += char_value
+        else:
+            out_number += char_value
+    return out_number
 
 
 class TestRomanConversion(unittest.TestCase):
@@ -40,6 +55,11 @@ class TestRomanConversion(unittest.TestCase):
     def test_CMI(self):
         input = convert_roman('CMI')
         output = 901
+        self.assertEqual(input, output)
+
+    def test_MMMCMXCIX(self):
+        input = convert_roman('MMMCMXCIX')
+        output = 3999
         self.assertEqual(input, output)
 
 
