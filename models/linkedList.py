fcceb53fa1e416e6b3c378dbb3f3c20fdf659e9e
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-13 13:36:10 -0700

    Add all_unique(), length(), to_set() methods

--- models/linkedList.py
+++ models/linkedList.py
@@ -23,11 +23,18 @@ class ListNode:
             current_node = self.next
         current_node.next = ListNode(new_val)
 
+    def all_unique(self) -> bool:
+        values = self.to_list()
+        return len(values) == len(set(values))
+
     def build(values: list):
         head = ListNode(values[0])
         for value in values[1:]:
             head.add(value)
         return head
+
+    def length(self) -> int:
+        return len(self.to_list())
 
     def to_list(self) -> list:
         values = list()
@@ -36,3 +43,6 @@ class ListNode:
             values.append(head.val)
             head = head.next
         return values
+
+    def to_set(self) -> set:
+        return set(self.to_list())
