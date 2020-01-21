e1d75812adf975f32e092153162da591d23d748a
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-21 08:08:20 -0700

    Add solution 58% faster than other python3 submissions

--- 58-Length-Last-Word.py
+++ 58-Length-Last-Word.py
@@ -1,13 +1,18 @@
 '''
 Psudocode:
-
+    Split into words
+    Return length of last word
 '''
 
 import unittest
 
 
 def len_last_word(s: str) -> int:
-    pass
+    words = s.split()
+    if words:
+        return len(words[-1])
+    else:
+        return 0
 
 
 class TestLastWordLength(unittest.TestCase):
@@ -23,7 +28,7 @@ class TestLastWordLength(unittest.TestCase):
         self.assertEqual(input, output)
 
     def test_All_your_base_are_belong_to_us(self):
-        input = len_last_word("All your base are belong to us")
+        input = len_last_word(" All your base are belong to us ")
         output = 2
         self.assertEqual(input, output)
 
@@ -31,3 +36,22 @@ class TestLastWordLength(unittest.TestCase):
         input = len_last_word("Hiya")
         output = 4
         self.assertEqual(input, output)
+
+    def test_blank(self):
+        input = len_last_word("")
+        output = 0
+        self.assertEqual(input, output)
+
+    def test_space(self):
+        input = len_last_word(" ")
+        output = 0
+        self.assertEqual(input, output)
+
+    def test_multi_space(self):
+        input = len_last_word("        ")
+        output = 0
+        self.assertEqual(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
