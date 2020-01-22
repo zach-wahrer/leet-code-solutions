f2102d5aab21b0c81cbd5380a86eff8ba78c4d08
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-21 19:47:56 -0700

    Add initial tests for 69-Sqrt(x)

---
+++ 69-Sqrt-x.py
@@ -0,0 +1,42 @@
+'''
+Psudocode:
+
+'''
+
+import unittest
+
+
+def square_root(x: int) -> int:
+    pass
+
+
+class TestSquareRoot(unittest.TestCase):
+
+    def test_4(self):
+        input = square_root(4)
+        output = 2
+        self.assertEqual(input, output)
+
+    def test_8(self):
+        input = square_root(8)
+        output = 2
+        self.assertEqual(input, output)
+
+    def test_99(self):
+        input = square_root(99)
+        output = 9
+        self.assertEqual(input, output)
+
+    def test_14cubed(self):
+        input = square_root(14**4)
+        output = 196
+        self.assertEqual(input, output)
+
+    def test_0(self):
+        input = square_root(0)
+        output = 0
+        self.assertEqual(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
