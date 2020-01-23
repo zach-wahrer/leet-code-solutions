643a1da6b1383e0ad61a7f8eb4dd922bd0164f58
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-23 15:01:42 -0700

    Add __iter__ for ListNode class

--- models/linkedList.py
+++ models/linkedList.py
@@ -17,6 +17,19 @@ class ListNode:
     def __ne__(self, other):
         return not self.__eq__(other)
 
+    def __iter__(self):
+        current = self
+        while current is not None:
+            yield current.val
+            current = current.next
+
+    # def __next__(self):
+    #     if self.next is not None:
+    #         value = self.val
+    #         self = self.next
+    #         return value
+    #     raise StopIteration
+
     def add(self, new_val):
         if self.val is None:
             self.val = new_val
