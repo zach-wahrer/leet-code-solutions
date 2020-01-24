f89cdd5b641c74c52c0a2b2542197ae8e46a4afd
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-24 07:49:59 -0700

    Add sort() then O(n) solution for 268-Missing-Number
    Faster than 5% of other python3 submissions.

--- 268-Missing-Number.py
+++ 268-Missing-Number.py
@@ -9,7 +9,14 @@ import unittest
 
 
 def what_is_missing(nums: list) -> int:
-    pass
+    nums.sort()
+    if nums[0] != 0:
+        return 0
+    for i, num in enumerate(nums):
+        if len(nums) == i + 1:
+            return num + 1
+        elif nums[i + 1] != num + 1:
+            return num + 1
 
 
 class TestMissing(unittest.TestCase):
@@ -27,12 +34,27 @@ class TestMissing(unittest.TestCase):
     def test_1to100(self):
         list_100 = [i for i in range(99)] + [100]
         input = what_is_missing(list_100)
-        output = 98
+        output = 99
         self.assertEqual(input, output)
 
     def test_4320(self):
         input = what_is_missing([4, 3, 2, 0])
         output = 1
+        self.assertEqual(input, output)
+
+    def test_0(self):
+        input = what_is_missing([0])
+        output = 1
+        self.assertEqual(input, output)
+
+    def test_10(self):
+        input = what_is_missing([1, 0])
+        output = 2
+        self.assertEqual(input, output)
+
+    def test_1(self):
+        input = what_is_missing([1])
+        output = 0
         self.assertEqual(input, output)
 
 
