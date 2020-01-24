80106180888750130afb8709884dc115a19d0d30
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-23 21:37:36 -0700

    Add tests for 217-Contains-Duplicate in list

---
+++ 217-Contains-Duplicate.py
@@ -0,0 +1,43 @@
+'''
+Psudocode:
+    Loop through list elments
+        Check if element is in list more than once
+'''
+
+import unittest
+
+
+def contains_duplicate(nums: list) -> bool:
+    pass
+
+
+class TestDuplicate(unittest.TestCase):
+    def test_1231(self):
+        input = contains_duplicate([1, 2, 3, 1])
+        output = True
+        self.assertEqual(input, output)
+
+    def test_1234(self):
+        input = contains_duplicate([1, 2, 3, 4])
+        output = False
+        self.assertEqual(input, output)
+
+    def test_1113343242(self):
+        input = contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
+        output = True
+        self.assertEqual(input, output)
+
+    def test_minus12344(self):
+        input = contains_duplicate([-1, -2, -3, -4, -4])
+        output = True
+        self.assertEqual(input, output)
+
+    def test_001(self):
+        input = contains_duplicate([0, 0, 1])
+        output = True
+        self.assertEqual(input, output)
+
+    def test_01511(self):
+        input = contains_duplicate([0, 1, 5, 11])
+        output = False
+        self.assertEqual(input, output)
