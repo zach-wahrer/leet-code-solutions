85a6057baf03f0be880cd66bb0394d782d85a9e6
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-26 16:20:39 -0700

    Add O(n^2) solution for 189-Rotate-Array

--- 189-Rotate-Array.py
+++ 189-Rotate-Array.py
@@ -6,8 +6,12 @@ Psudocode:
 import unittest
 
 
+# Brute O(n^2)
 def rotate_array(nums: list, k: int) -> None:
-    pass
+    for i in range(k):
+        nums.insert(0, nums[-1])
+        nums.pop(-1)
+    return nums
 
 
 class TestRotateArray(unittest.TestCase):
@@ -34,8 +38,14 @@ class TestRotateArray(unittest.TestCase):
 
     def test_123_2(self):
         input = rotate_array([1, 2, 3], 2)
-        output = [3, 1, 2]
+        output = [2, 3, 1]
         self.assertEqual(input, output)
 
-    if __name__ == "__main__":
-        unittest.main()
+    def test_negatives(self):
+        input = rotate_array([-1, -100, 3, 99], 2)
+        output = [3, 99, -1, -100]
+        self.assertEqual(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
