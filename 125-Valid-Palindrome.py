854b53c98e8bea2a8c73312f014db447388d6af9
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-01 17:47:16 -0700

    Add 125-Valid-Palindrome tests

---
+++ 125-Valid-Palindrome.py
@@ -0,0 +1,24 @@
+'''
+Psudocode:
+
+'''
+
+import unittest
+
+
+def valid_palindrome(s: str) -> bool:
+    pass
+
+
+class TestPalindrome(unittest.TestCase):
+    def test_a_man(self):
+        input = valid_palindrome("A man, a plan, a canal: Panama")
+        self.assertTrue(input)
+
+    def test_race(self):
+        input = valid_palindrome("race a car")
+        self.assertFalse(input)
+
+
+if __name__ == '__main__':
+    unittest.main()
