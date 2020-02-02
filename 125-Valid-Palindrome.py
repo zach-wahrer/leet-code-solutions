019a7a976376e12f5cadfc600e616b3ce2110b9e
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-02 12:42:07 -0700

    Add additional tests for 125-Valid-Palindrome

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -15,8 +15,20 @@ class TestPalindrome(unittest.TestCase):
         input = valid_palindrome("A man, a plan, a canal: Panama")
         self.assertTrue(input)
 
+    def test_radar(self):
+        input = valid_palindrome("ra12dar")
+        self.assertTrue(input)
+
+    def test_radar(self):
+        input = valid_palindrome("race car")
+        self.assertTrue(input)
+
     def test_race(self):
         input = valid_palindrome("race a car")
+        self.assertFalse(input)
+
+    def test_heart(self):
+        input = valid_palindrome("I heart sea food")
         self.assertFalse(input)
 
 
