366d5c6aff8dfcd19cfd3b67c11cdf07c7652d9c
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-12 13:43:49 -0700

    Add solutions that don't meet space or speed requirements for 422-Find-All-Duplicates

--- medium/422-Find-All-Duplicates-In-Array.py
+++ medium/422-Find-All-Duplicates-In-Array.py
@@ -1,8 +1,34 @@
 import unittest
 
 
+# Naive solution, O(n) space and time
+# Loop through the nums, checking if we've seen them before
+# If so, output to a list of duplicates
+def find_all_duplicates_set(nums: list) -> list:
+    seen = set()
+    dupes = []
+    for i in nums:
+        if i in seen and i not in dupes:
+            dupes.append(i)
+        else:
+            seen.add(i)
+    return dupes
+
+
+# Two pointer solution, O(n(logn)) time, O(1) space
+# Exceeds time limit on leet code
 def find_all_duplicates(nums: list) -> list:
-    pass
+    left = 0
+    right = 1
+    dupes = []
+    while left < len(nums):
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                dupes.append(nums[left])
+            right += 1
+        left += 1
+        right = left + 1
+    return dupes
 
 
 class TestAllDuplicates(unittest.TestCase):
@@ -29,7 +55,7 @@ class TestAllDuplicates(unittest.TestCase):
 
     def test_98765511(self):
         input = find_all_duplicates([9, 8, 7, 8, 5, 5, 1, 1])
-        self.assertEqual(input, [5, 1])
+        self.assertEqual(input, [8, 5, 1])
 
 
 if __name__ == "__main__":
