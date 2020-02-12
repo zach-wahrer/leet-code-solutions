192765757bf4d7257dc3e15b317d3b14f9574da6
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-12 10:52:08 -0700

    Move files into orginization folders

--- 1108-Defang-IP-Address.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def defangIPaddr(self, address: str) -> str:
-
-        a = address.split(".")
-        f = "[.]"
-        defang = (a[0] + f + a[1] + f + a[2] + f + a[3])
-        return defang
