925e31526029a69c2f7adac9ad366f21f633ec6e
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-15 17:54:20 -0700

    Add O(n) solutions for 1342-Number-Steps-Reduce-Number-Zero

--- easy/1342-Number-Steps-Reduce-Number-Zero.py
+++ easy/1342-Number-Steps-Reduce-Number-Zero.py
@@ -1,8 +1,42 @@
 import unittest
 
 
+# Naive O(n) solution
+def reduce_num_to_zero_naive(num: int) -> int:
+
+    if num == "":
+        return 0
+
+    count = 0
+    while num > 0:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num -= 1
+        count += 1
+
+    return count
+
+
+# Recursive O(n) solution
 def reduce_num_to_zero(num: int) -> int:
-    pass
+
+    if num == "":
+        return 0
+
+    count = 0
+
+    def _reduce_num_to_zero(num, count):
+        if num == 0:
+            return count
+
+        count += 1
+        if num % 2 == 0:
+            return _reduce_num_to_zero(num / 2, count)
+        else:
+            return _reduce_num_to_zero(num - 1, count)
+
+    return _reduce_num_to_zero(num, count)
 
 
 class TestReduceToZero(unittest.TestCase):
@@ -26,3 +60,7 @@ class TestReduceToZero(unittest.TestCase):
     def test_0(self):
         input = reduce_num_to_zero(0)
         self.assertEqual(input, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
