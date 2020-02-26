6cd50934b3c4a1b77a24edd309f7b2adf27b3b30
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-26 16:51:35 -0700

    Add O(n**2) deque solution for 3-Longest-Substring-WO-Repeating-Chars

--- medium/3-Longest-Substring-WO-Repeating-Chars.py
+++ medium/3-Longest-Substring-WO-Repeating-Chars.py
@@ -1,3 +1,4 @@
+from collections import deque
 import unittest
 
 
@@ -20,8 +21,10 @@ def longest_sub_naive(s: str) -> int:
 
     return max(counts)
 
+# Recursive moving window - fails most tests
 
-def longest_sub(s: str) -> int:
+
+def longest_sub_recursive(s: str) -> int:
     if len(s) < 2:
         return len(s)
     counts = [0]
@@ -33,9 +36,9 @@ def longest_sub(s: str) -> int:
             while s[left] != s[right] and right < len(s) - 1:
                 right += 1
             counts.append(right - left) if left == 0 else counts.append(right - left + 1)
-            print(right - left)
+            print(right - left, ":", left, s[left], "-", right, s[right])
             left += 1
-            print(left, s[left], " : ", right, s[right])
+            print("Sending to recursive: ", left, s[left], " : ", right, s[right])
 
             move_window(left, right, counts)
 
@@ -45,34 +48,69 @@ def longest_sub(s: str) -> int:
     return max(counts)
 
 
+# Deque solution
+def longest_sub(s: str) -> int:
+
+    if len(s) < 2:
+        return len(s)
+
+    char_deque = deque()
+    char_deque.append(s[0])
+    counts = [0]
+
+    for char in s[1:]:
+        if char == char_deque[0]:
+            counts.append(len(char_deque))
+            char_deque.popleft()
+            char_deque.append(char)
+        elif char in char_deque:
+            counts.append(len(char_deque))
+            for i in range(char_deque.index(char) + 1):
+                char_deque.popleft()
+            char_deque.append(char)
+        else:
+            char_deque.append(char)
+
+    if char_deque:
+        counts.append(len(char_deque))
+
+    return max(counts)
+
+
 class TestLongestSubstring(unittest.TestCase):
 
     def test_dvdf_3(self):
         self.assertEqual(longest_sub('dvdf'), 3)
-    #
-    # def test_abcabcbb_3(self):
-    #     self.assertEqual(longest_sub('abcabcbb'), 3)
-    #
-    # def test_bbbbb_1(self):
-    #     self.assertEqual(longest_sub('bbbbb'), 1)
-    #
-    # def test_pwwkew_3(self):
-    #     self.assertEqual(longest_sub('pwwkew'), 3)
-    #
-    # def test_blank_0(self):
-    #     self.assertEqual(longest_sub(''), 0)
-    #
-    # def test_a_1(self):
-    #     self.assertEqual(longest_sub('a'), 1)
-    #
-    # def test_ab_2(self):
-    #     self.assertEqual(longest_sub('ab'), 2)
-    #
-    # def test_ab1ab1_3(self):
-    #     self.assertEqual(longest_sub('ab1ab1'), 3)
-    #
-    # def test_aab_2(self):
-    #     self.assertEqual(longest_sub('aab'), 2)
+
+    def test_abcabcbb_3(self):
+        self.assertEqual(longest_sub('abcabcbb'), 3)
+
+    def test_bbbbb_1(self):
+        self.assertEqual(longest_sub('bbbbb'), 1)
+
+    def test_pwwkew_3(self):
+        self.assertEqual(longest_sub('pwwkew'), 3)
+
+    def test_blank_0(self):
+        self.assertEqual(longest_sub(''), 0)
+
+    def test_a_1(self):
+        self.assertEqual(longest_sub('a'), 1)
+
+    def test_ab_2(self):
+        self.assertEqual(longest_sub('ab'), 2)
+
+    def test_ab1ab1_3(self):
+        self.assertEqual(longest_sub('ab1ab1'), 3)
+
+    def test_aab_2(self):
+        self.assertEqual(longest_sub('aab'), 2)
+
+    def test_cdd_2(self):
+        self.assertEqual(longest_sub('cdd'), 2)
+
+    def test_ohvhjdml_5(self):
+        self.assertEqual(longest_sub('ohvhjdml'), 6)
 
 
 if __name__ == "__main__":
