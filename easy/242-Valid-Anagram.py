9555c4424e61cb4e08a9c33d3b70de080febfd7d
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-26 15:10:54 -0700

    Add naive and O(n+m) solutions for 242-Valid-Anagram

--- easy/242-Valid-Anagram.py
+++ easy/242-Valid-Anagram.py
@@ -1,8 +1,33 @@
 import unittest
 
 
+# Naive solution - O(n**2) - Doesn't pass test_multi_false
+def is_anagram_naive(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    for char in s:
+        if char not in t:
+            return False
+
+    return True
+
+
+# Solution - O(n+m) time, O(n+m) space
 def is_anagram(s: str, t: str) -> bool:
-    pass
+    if len(s) != len(t):
+        return False
+
+    def element_counter(string):
+        counts = {}
+        for char in string:
+            if char in counts:
+                counts[char] += 1
+            else:
+                counts[char] = 1
+        return counts
+
+    return element_counter(s) == element_counter(t)
 
 
 class TestAnagram(unittest.TestCase):
@@ -24,3 +49,16 @@ class TestAnagram(unittest.TestCase):
 
     def test_op_true(self):
         self.assertTrue(is_anagram('op', 'po'))
+
+    def test_multi_true(self):
+        self.assertTrue(is_anagram('aaz', 'zaa'))
+
+    def test_multi_false(self):
+        self.assertFalse(is_anagram('azz', 'zaa'))
+
+    def test_diff_len_false(self):
+        self.assertFalse(is_anagram('abc', 'abcd'))
+
+
+if __name__ == "__main__":
+    unittest.main()
