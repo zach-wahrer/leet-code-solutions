a17f4312ded6b4ad2e3fb9640740a1eac8391ae9
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-28 17:15:56 -0700

    Fix typo in test_binary_tree

--- easy/models/test_binary_tree.py
+++ easy/models/test_binary_tree.py
@@ -6,6 +6,6 @@ print('Tree1:')
 preorder(tree)
 
 tree2 = TreeNode()
-tree2.build_from_heap([3, 9, 20, None, None, 15, 7])
+tree2.build_from_heap([3, 9, 20, None, 1, 15, 7])
 print('\nTree2:')
-preorder(tree)
+preorder(tree2)
