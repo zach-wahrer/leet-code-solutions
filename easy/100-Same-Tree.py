170c7a682007c15d8c01a595b28338e501c952b2
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-29 16:08:30 -0700

    Add recursive solution for 100-Same-Tree.py

--- easy/100-Same-Tree.py
+++ easy/100-Same-Tree.py
@@ -3,7 +3,25 @@ from models.binary_tree import TreeNode
 
 
 def same_tree(p: TreeNode, q: TreeNode) -> bool:
-    pass
+    if not p and not q:
+        return True
+
+    if not p and q or not q and p:
+        return False
+
+    def dfs_tree(node: TreeNode) -> TreeNode:
+        yield node
+        if node.left:
+            yield from dfs_tree(node.left)
+        if node.right:
+            yield from dfs_tree(node.right)
+
+    for tree1, tree2 in zip(dfs_tree(p), dfs_tree(q)):
+        if tree1.val != tree2.val or bool(tree1.left) != bool(tree2.left) or \
+                bool(tree1.right) != bool(tree2.right):
+            return False
+
+    return True
 
 
 class TestSameTree(unittest.TestCase):
@@ -55,4 +73,8 @@ class TestSameTree(unittest.TestCase):
         tree2 = TreeNode()
         tree1.build_from_heap([1])
         tree2.build_from_heap([2])
-        self.assertTrue(same_tree(tree1, tree2))
+        self.assertFalse(same_tree(tree1, tree2))
+
+
+if __name__ == "__main__":
+    unittest.main()
