ad0a848176771a92018280b892d31333b8b296dc
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-05 11:54:30 -0700

    Add O(n + m) and binary search solutions for 1351-Negative-Nums-In-Sorted-Matrix.py

--- easy/1351-Negative-Nums-In-Sorted-Matrix.py
+++ easy/1351-Negative-Nums-In-Sorted-Matrix.py
@@ -1,8 +1,55 @@
 import unittest
 
 
+# Naive O(n + m) time, O(1) space
+def count_negatives_simple(grid: list) -> int:
+    counter = 0
+    for row in grid:
+        for number in row:
+            if number < 0:
+                counter += 1
+    return counter
+
+
+# Binary seach O(log(n) * m) time, O(1) space
 def count_negatives(grid: list) -> int:
-    pass
+    counter = 0
+
+    if not grid[0]:
+        return counter
+
+    on_row = 0
+
+    while on_row < len(grid):
+        left = 0
+        right = len(grid[0]) - 1
+        whole_row = False
+        first_negative = None
+
+        if grid[on_row][right] >= 0:
+            on_row += 1
+            continue
+        if grid[on_row][left] < 0:
+            first_negative = 0
+            whole_row = True
+
+        while left <= right and whole_row is False:
+            middle = (right + left) // 2
+            if grid[on_row][middle] < 0 and grid[on_row][middle - 1] >= 0:
+                first_negative = middle
+                break
+
+            if grid[on_row][middle] >= 0:
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        if first_negative >= 0:
+            counter += len(grid[on_row]) - first_negative
+
+        on_row += 1
+
+    return counter
 
 
 class TestNegativesInMaxtrix(unittest.TestCase):
@@ -17,7 +64,7 @@ class TestNegativesInMaxtrix(unittest.TestCase):
 
     def test_3(self):
         input = [[1, -1], [-1, -1]]
-        self.assertEqual(count_negatives(input), 8)
+        self.assertEqual(count_negatives(input), 3)
 
     def test_1(self):
         input = [[-1]]
