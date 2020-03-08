44095ca7d2bd0cd1814672acda52d55cc1732273
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-07 17:17:54 -0700

    Update tests to be correct for 112-Path-Sum.py

--- easy/112-Path-Sum.py
+++ easy/112-Path-Sum.py
@@ -2,6 +2,7 @@ import unittest
 from models.binarytree import TreeNode
 
 
+# Recursive
 def path_sum(root: TreeNode, sum: int) -> bool:
     pass
 
@@ -9,15 +10,21 @@ def path_sum(root: TreeNode, sum: int) -> bool:
 class TestPathSum(unittest.TestCase):
 
     def test_22_true(self):
-        tree = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, None, None, 1]
+        tree = TreeNode()
+        tree.build_from_heap([5, 4, 8, 11, None, 13, 4, 7, 2,
+                              None, None, None, None, None, 1])
         self.assertTrue(path_sum(tree, 22))
 
     def test_26_true(self):
-        tree = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, None, None, 1]
+        tree = TreeNode()
+        tree.build_from_heap([5, 4, 8, 11, None, 13, 4, 7, 2,
+                              None, None, None, None, None, 1])
         self.assertTrue(path_sum(tree, 26))
 
     def test_1000_false(self):
-        tree = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, None, None, 1]
+        tree = TreeNode()
+        tree.build_from_heap([5, 4, 8, 11, None, 13, 4, 7, 2,
+                              None, None, None, None, None, 1])
         self.assertFalse(path_sum(tree, 1000))
 
     def test_1_true(self):
