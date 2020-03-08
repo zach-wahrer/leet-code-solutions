4ac5a1b593a21b0e70391108b52a6a66b18f2e06
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-08 12:17:31 -0600

    Add recursive solution for 113-Path-Sum-II.py

--- medium/113-Path-Sum-II.py
+++ medium/113-Path-Sum-II.py
@@ -2,8 +2,28 @@ import unittest
 from models.binary_tree import TreeNode
 
 
+# Recursive
 def path_sum(root: TreeNode, sum: int) -> list:
-    pass
+    if not root:
+        return []
+
+    final_paths = []
+
+    def _path_sum(node, acc_sum, curr_path, req_sum):
+        if not node.left and not node.right:
+            if node.val + acc_sum == req_sum:
+                final_paths.append(curr_path + [node.val])
+                return
+
+        if node.left:
+            _path_sum(node.left, acc_sum + node.val, curr_path + [node.val], req_sum)
+
+        if node.right:
+            _path_sum(node.right, acc_sum + node.val, curr_path + [node.val], req_sum)
+
+    _path_sum(root, 0, [], sum)
+
+    return final_paths
 
 
 class TestPathSum(unittest.TestCase):
@@ -18,7 +38,7 @@ class TestPathSum(unittest.TestCase):
         tree = TreeNode()
         tree.build_from_heap([6, 4, 8, 11, None, 13, 4, 7, 2,
                               None, None, None, None, 5, 1])
-        self.assertEqual(path_sum(tree, 22), [[]])
+        self.assertEqual(path_sum(tree, 22), [])
 
     def test_one_path_26(self):
         tree = TreeNode()
