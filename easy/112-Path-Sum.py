04a0c643556f0b840eb262dc2a664e4c3f4c59e8
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-07 17:40:05 -0700

    Add recursive solution for 112-Path-Sum.py

--- easy/112-Path-Sum.py
+++ easy/112-Path-Sum.py
@@ -1,10 +1,27 @@
 import unittest
-from models.binarytree import TreeNode
+from models.binary_tree import TreeNode
 
 
 # Recursive
 def path_sum(root: TreeNode, sum: int) -> bool:
-    pass
+
+    if not root:
+        return False
+
+    sums = []
+
+    def _path_sum(node, acc):
+        if not node.left and not node.right:
+            sums.append(acc + node.val)
+            return
+        if node.left:
+            _path_sum(node.left, acc + node.val)
+        if node.right:
+            _path_sum(node.right, acc + node.val)
+
+    _path_sum(root, 0)
+
+    return sum in sums
 
 
 class TestPathSum(unittest.TestCase):
@@ -28,13 +45,20 @@ class TestPathSum(unittest.TestCase):
         self.assertFalse(path_sum(tree, 1000))
 
     def test_1_true(self):
-        tree = [1]
+        tree = TreeNode()
+        tree.build_from_heap([1])
         self.assertTrue(path_sum(tree, 1))
 
     def test_1_false(self):
-        tree = [1]
+        tree = TreeNode()
+        tree.build_from_heap([1])
         self.assertFalse(path_sum(tree, 20))
 
     def test_blank_false(self):
-        tree = []
+        tree = TreeNode()
+        tree.build_from_heap([])
         self.assertFalse(path_sum(tree, 14))
+
+
+if __name__ == "__main__":
+    unittest.main()
