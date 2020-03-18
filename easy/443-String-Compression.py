bdd1c7c9bdf69081613e2ac421c3ecad22950ad9
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-18 14:38:23 -0600

    Update tests for 443-String-Compression.py

--- easy/443-String-Compression.py
+++ easy/443-String-Compression.py
@@ -10,7 +10,7 @@ class TestCompress(unittest.TestCase):
     def test_a2b2c3(self):
         string = ['a', 'a', 'b', 'b', 'c', 'c', 'c']
         input = compress_string(string)
-        self.assertEqual((input, string), (6, ['a', '2', 'b', '2', 'c', '3', '']))
+        self.assertEqual((input, string), (6, ['a', '2', 'b', '2', 'c', '3', 'c']))
 
     def test_a(self):
         string = ['a']
@@ -21,7 +21,7 @@ class TestCompress(unittest.TestCase):
         string = ["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]
         input = compress_string(string)
         self.assertEqual(
-            (input, string), (4, ["a", "b", "1", "2", "", "", "", "", "", "", "", "", ""]))
+            (input, string), (4, ["a", "b", "1", "2", "b", "b", "b", "b", "b", "b", "b", "b", "b"]))
 
 
 if __name__ == '__main__':
