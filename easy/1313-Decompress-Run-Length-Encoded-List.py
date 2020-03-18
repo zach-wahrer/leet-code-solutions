ff3b9d9ca436506a834e2e5d8be3f1a491f8061a
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-18 14:26:18 -0600

    Add O(n) solution for 1313-Decompress-Run-Length-Encoded-List.py

--- easy/1313-Decompress-Run-Length-Encoded-List.py
+++ easy/1313-Decompress-Run-Length-Encoded-List.py
@@ -1,8 +1,18 @@
 import unittest
 
 
+# O(n) solution
 def decompress_list(nums: list) -> list:
-    pass
+    output = []
+    p1, p2 = 0, 1
+
+    while p2 < len(nums):
+        for _ in range(nums[p1]):
+            output.append(nums[p2])
+        p1 += 2
+        p2 += 2
+
+    return output
 
 
 class TestDecompress(unittest.TestCase):
@@ -18,3 +28,7 @@ class TestDecompress(unittest.TestCase):
     def test_11(self):
         input = decompress_list([1, 1])
         self.assertEqual(input, [1])
+
+
+if __name__ == '__main__':
+    unittest.main()
