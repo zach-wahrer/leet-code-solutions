af4f3d400337f814ba1afba23150a1a992a31369
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-22 16:47:15 -0600

    Add O(n) time/space solution for 412-Fizz-Buzz.py

--- easy/412-Fizz-Buzz.py
+++ easy/412-Fizz-Buzz.py
@@ -1,8 +1,37 @@
 import unittest
 
 
+# O(n) time/space solution
 def fizz_buzz(n: int) -> list:
-    pass
+    output = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            output.append('FizzBuzz')
+        elif i % 3 == 0:
+            output.append('Fizz')
+        elif i % 5 == 0:
+            output.append('Buzz')
+        else:
+            output.append(str(i))
+
+    return output
+
+
+# Reorganized O(n) time/space solution
+def fizz_buzz(n: int) -> list:
+    output = []
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                output.append('FizzBuzz')
+            else:
+                output.append('Fizz')
+        elif i % 5 == 0:
+            output.append('Buzz')
+        else:
+            output.append(str(i))
+
+    return output
 
 
 class TestFizzBuzz(unittest.TestCase):
@@ -27,10 +56,12 @@ class TestFizzBuzz(unittest.TestCase):
         ]
         self.assertEqual(fizz_buzz(15), output)
 
+    def test_blank(self):
+        self.assertEqual(fizz_buzz(0), [])
 
-def test_blank(self):
-    self.assertEqual(fizz_buzz(0), [])
+    def test_1(self):
+        self.assertEqual(fizz_buzz(1), ['1'])
 
 
-def test_1(self):
-    self.assertEqual(fizz_buzz(1), ['1'])
+if __name__ == "__main__":
+    unittest.main()
