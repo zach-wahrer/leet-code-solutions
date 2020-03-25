d501e9698ef86d936ac287d509b6e0f1815f90ac
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-25 07:57:24 -0600

    Add quadradic solution for 387-First-Unique-Char-In-String.py

--- easy/387-First-Unique-Char-In-String.py
+++ easy/387-First-Unique-Char-In-String.py
@@ -1,8 +1,18 @@
 import unittest
 
 
+# O(n**2) time, O(n) space solution
 def first_unique(s: str) -> int:
-    pass
+    from collections import Counter
+
+    counts = Counter(s)
+
+    unique_positions = []
+    for i in counts:
+        if counts[i] == 1:
+            unique_positions.append(s.index(i))
+
+    return min(unique_positions) if unique_positions else -1
 
 
 class TestFirstUnique(unittest.TestCase):
