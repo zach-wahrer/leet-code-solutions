712eda2010bcaead9affcce2e8153b2602d1e634
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-28 11:55:57 -0600

    Add timed out solution for 70-Climbing-Stairs.py

--- unsolved/70-Climbing-Stairs.py
+++ unsolved/70-Climbing-Stairs.py
@@ -1,13 +1,18 @@
-'''
-Psudocode:
-
-'''
-
 import unittest
 
 
 def how_many_ways(n: int) -> int:
-    pass
+
+    def _climb(remaining_stairs, steps):
+        if remaining_stairs == 0:
+            return 1
+
+        if remaining_stairs < 0:
+            return 0
+
+        return _climb(remaining_stairs - 1, 1) + _climb(remaining_stairs - 2, 2)
+
+    return _climb(n, 1)
 
 
 class TestWaysToClimb(unittest.TestCase):
@@ -24,7 +29,17 @@ class TestWaysToClimb(unittest.TestCase):
 
     def test_4(self):
         input = how_many_ways(4)
-        output = 4
+        output = 5
+        self.assertEqual(input, output)
+
+    def test_1(self):
+        input = how_many_ways(1)
+        output = 1
+        self.assertEqual(input, output)
+
+    def test_38(self):
+        input = how_many_ways(38)
+        output = 63245986
         self.assertEqual(input, output)
 
 
