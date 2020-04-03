36478c345b78ca7aa69f14b1ccc3731dbd82f6ed
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-03 08:52:50 -0600

    Add iterative O(n) time solution for 3-Maximum-Subarray.py

--- 30-day-challenge/week1/3-Maximum-Subarray.py
+++ 30-day-challenge/week1/3-Maximum-Subarray.py
@@ -1,11 +1,32 @@
 import unittest
 
 
+# Iterative O(n) time, O(1) space solution
 def max_subarray(nums: list) -> int:
-    pass
+    tmp_sum = nums[0]
+    max_sum = nums[0]
+
+    for i in nums[1:]:
+        tmp_sum = (max(tmp_sum + i, i))
+        if tmp_sum > max_sum:
+            max_sum = tmp_sum
+
+    return max_sum
 
 
 class TestMaxSubarray(unittest.TestCase):
     def test_long(self):
         sub = max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
         self.assertEqual(sub, 6)
+
+    def test_short(self):
+        sub = max_subarray([-2, 1])
+        self.assertEqual(sub, 1)
+
+    def test_one(self):
+        sub = max_subarray([-2])
+        self.assertEqual(sub, -2)
+
+
+if __name__ == "__main__":
+    unittest.main()
