14000e1111665671b69578376795b5e5a9fb407a
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-07 09:35:01 -0600

    Add O(n log(n)) time / O(1) space solution for 7-Counting-Elements.py

--- 30-day-challenge/week1/7-Counting-Elements.py
+++ 30-day-challenge/week1/7-Counting-Elements.py
@@ -1,11 +1,37 @@
 import unittest
 
 
+# O(n log n) time / O(1) space solution
 def count_elements(arr: list) -> int:
-    pass
+    arr.sort()
+    total_count, provisional_count = 0, 1
+    p1, p2 = 0, 1
+
+    while p2 < len(arr):
+        while p2 < len(arr) and arr[p1] == arr[p2]:
+            provisional_count += 1
+            p2 += 1
+        if p2 < len(arr) and arr[p1] + 1 == arr[p2]:
+            if provisional_count > 1:
+                total_count += provisional_count
+                provisional_count = 1
+            else:
+                total_count += 1
+
+        provisional_count = 1
+        p1 = p2
+        p2 += 1
+
+    return total_count
 
 
 class TestCounts(unittest.TestCase):
+    def test_leet2(self):
+        self.assertEqual(count_elements([0, 6, 3, 4, 2, 10, 4, 3, 10, 0]), 3)
+
+    def test_leet(self):
+        self.assertEqual(count_elements([2, 9, 0, 7, 6, 2, 7, 7, 0]), 1)
+
     def test_short(self):
         self.assertEqual(count_elements([1, 2, 3]), 2)
 
@@ -16,4 +42,8 @@ class TestCounts(unittest.TestCase):
         self.assertEqual(count_elements([1, 3, 2, 3, 5, 0]), 3)
 
     def test_duplicates(self):
-        self.assertEqual(count_elements([1, 1, 2, 2]), )
+        self.assertEqual(count_elements([1, 1, 2, 2]), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
