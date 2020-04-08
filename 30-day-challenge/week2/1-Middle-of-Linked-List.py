a178083435e6294a5cf45c7279ff9316aa19b8c9
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-08 09:20:32 -0600

    Add O(n) time / O(1) space solution for 1-Middle-of-Linked-List.py

--- 30-day-challenge/week2/1-Middle-of-Linked-List.py
+++ 30-day-challenge/week2/1-Middle-of-Linked-List.py
@@ -2,15 +2,32 @@ from linkedList import ListNode
 import unittest
 
 
+# O(n) time / O(1) space solution
 def get_middle(head: ListNode) -> ListNode:
-    pass
+    node = head
+    length = 0
+    while node:
+        length += 1
+        node = node.next
+
+    middle = length // 2
+
+    node = head
+    for _ in range(middle):
+        node = node.next
+
+    return node
 
 
 class TestMiddle(unittest.TestCase):
     def test_12345(self):
-        head = ListNode().build([1, 2, 3, 4, 5])
+        head = ListNode.build([1, 2, 3, 4, 5])
         self.assertEqual(get_middle(head), head.next.next)
 
     def test_123456(self):
-        head = ListNode().build([1, 2, 3, 4, 5, 6])
+        head = ListNode.build([1, 2, 3, 4, 5, 6])
         self.assertEqual(get_middle(head), head.next.next.next)
+
+
+if __name__ == "__main__":
+    unittest.main()
