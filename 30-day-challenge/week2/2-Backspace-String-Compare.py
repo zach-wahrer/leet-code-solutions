24a6b7c3060a60af8d197b089ddd6da3670eae49
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-09 09:38:11 -0600

    Add O(n + m) time / space solution for 2-Backspace-String-Compare.py

--- 30-day-challenge/week2/2-Backspace-String-Compare.py
+++ 30-day-challenge/week2/2-Backspace-String-Compare.py
@@ -2,7 +2,17 @@ import unittest
 
 
 def compare_strings(S: str, T: str) -> bool:
-    pass
+
+    def process_string(string) -> list:
+        stack = []
+        for i in string[::]:
+            if i == "#" and stack:
+                stack.pop()
+            elif i != "#":
+                stack.append(i)
+        return stack
+
+    return process_string(S) == process_string(T)
 
 
 class TestStringCompare(unittest.TestCase):
@@ -24,6 +34,11 @@ class TestStringCompare(unittest.TestCase):
     def test_cb_false(self):
         S = "a#c"
         T = "b"
+        self.assertFalse(compare_strings(S, T))
+
+    def test_y_true(self):
+        S = "y#fo##f"
+        T = "y#f#o##f"
         self.assertTrue(compare_strings(S, T))
 
 
