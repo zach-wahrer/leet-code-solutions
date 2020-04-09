e33a5c4e58b8d1c0e7d29c0eb69af771acdac822
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-09 10:39:14 -0600

    Add two pointer partial solution for 2-Backspace-String-Compare.py

--- 30-day-challenge/week2/2-Backspace-String-Compare.py
+++ 30-day-challenge/week2/2-Backspace-String-Compare.py
@@ -1,7 +1,8 @@
 import unittest
 
 
-def compare_strings(S: str, T: str) -> bool:
+# O(n + m) time / space solution
+def compare_strings_stack(S: str, T: str) -> bool:
 
     def process_string(string) -> list:
         stack = []
@@ -15,7 +16,60 @@ def compare_strings(S: str, T: str) -> bool:
     return process_string(S) == process_string(T)
 
 
+# Two pointer O(1) space solution, doesn't work for all test cases
+def compare_strings(S: str, T: str) -> bool:
+    p1, p2 = len(S) - 1, len(T) - 1
+    S_skip, T_skip = 0, 0
+
+    while p1 >= 0 and p2 >= 0:
+
+        while p1 >= 0:
+            if S[p1] == "#":
+                S_skip += 1
+                p1 -= 1
+            else:
+                if S_skip:
+                    S_skip -= 1
+                    p1 -= 1
+                else:
+                    break
+
+        while p2 >= 0:
+            if T[p2] == "#":
+                T_skip += 1
+                p2 -= 1
+            else:
+                if T_skip:
+                    T_skip -= 1
+                    p2 -= 1
+                else:
+                    break
+
+        if S[p1] != T[p2]:
+            return False
+
+        p1 -= 1
+        p2 -= 1
+
+    return True
+
+
 class TestStringCompare(unittest.TestCase):
+    def test_ac_leet3(self):
+        S = "nzp#o#g"
+        T = "b#nzp#o#g"
+        self.assertTrue(compare_strings(S, T))
+
+    def test_ac_leet2(self):
+        S = "bbbextm"
+        T = "bbb#extm"
+        self.assertFalse(compare_strings(S, T))
+
+    def test_ac_leet(self):
+        S = "isfcow#"
+        T = "isfcog#w#"
+        self.assertTrue(compare_strings(S, T))
+
     def test_ac_true(self):
         S = "ab#c"
         T = "ad#c"
