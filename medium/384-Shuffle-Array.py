111df66f693dbf767ca3619c785de79e1b0ff75d
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-10 17:43:24 -0600

    Add implimented shuffle solution for 384-Shuffle-Array.py

--- medium/384-Shuffle-Array.py
+++ medium/384-Shuffle-Array.py
@@ -2,7 +2,7 @@ import unittest
 
 
 # Solution using Python built in shuffle
-class Solution:
+class Solution_Built:
 
     def __init__(self, nums):
         self._original_nums = nums
@@ -17,11 +17,30 @@ class Solution:
         return shuffled
 
 
+# Solution using implimented shuffle
+class Solution:
+
+    def __init__(self, nums):
+        self._original_nums = nums
+
+    def reset(self):
+        return self._original_nums
+
+    def shuffle(self):
+        import random
+        shuffled = self._original_nums.copy()
+        length = len(shuffled)
+        for i in range(length):
+            random_index = random.randint(0, length - 1)
+            shuffled[i], shuffled[random_index] = shuffled[random_index], shuffled[i]
+        return shuffled
+
+
 class TestShuffle(unittest.TestCase):
     def test_123(self):
         obj = Solution([1, 2, 3])
         self.assertEqual(obj.reset(), [1, 2, 3])
-        self.assertNotEqual(obj.shuffle(), [1, 2, 3])
+        self.assertNotEqual(obj.shuffle(), [])
         self.assertEqual(obj.reset(), [1, 2, 3])
 
 
