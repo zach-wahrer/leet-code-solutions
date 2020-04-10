54f57312b53129885d78b84ee0a85a70a2337d95
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-10 17:29:30 -0600

    Add solution for 384-Shuffle-Array.py

---
+++ medium/384-Shuffle-Array.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+# Solution using Python built in shuffle
+class Solution:
+
+    def __init__(self, nums):
+        self._original_nums = nums
+        self._random_nums = None
+
+    def reset(self):
+        return self._original_nums
+
+    def shuffle(self):
+        import random
+        shuffled = self._original_nums.copy()
+        random.shuffle(shuffled)
+        return shuffled
+
+
+class TestShuffle(unittest.TestCase):
+    def test_123(self):
+        obj = Solution([1, 2, 3])
+        self.assertEqual(obj.reset(), [1, 2, 3])
+        self.assertNotEqual(obj.shuffle(), [1, 2, 3])
+        self.assertEqual(obj.reset(), [1, 2, 3])
+
+
+if __name__ == "__main__":
+    unittest.main()
