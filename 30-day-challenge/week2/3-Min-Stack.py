8faa446c4def02f29ed53d7d2f90beb145dc0687
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-10 09:52:16 -0600

    Add solution for 3-Min-Stack.py

--- 30-day-challenge/week2/3-Min-Stack.py
+++ 30-day-challenge/week2/3-Min-Stack.py
@@ -4,22 +4,27 @@ import unittest
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        pass
+        self._min_val = float("inf")
+        self._values = []
 
     def push(self, x: int) -> None:
-        pass
+        self._values.append(x)
+        if x < self._min_val:
+            self._min_val = x
 
     def pop(self) -> None:
-        pass
+        value = self._values.pop()
+        if value == self._min_val and self._values:
+            self._min_val = min(self._values)
+        if not self._values:
+            self._min_val = float("inf")
+        return value
 
     def top(self) -> int:
-        pass
+        return self._values[-1]
 
     def getMin(self) -> int:
-        pass
+        return self._min_val
 
 
 # Your MinStack object will be instantiated and called as such:
@@ -39,6 +44,14 @@ class TestMinStack(unittest.TestCase):
         stack.pop()
         self.assertEqual(stack.top(), 0)
         self.assertEqual(stack.getMin(), -2)
+
+    def test_min_2(self):
+        stack = MinStack()
+        stack.push(-2)
+        self.assertEqual(stack.getMin(), -2)
+        self.assertEqual(stack.pop(), -2)
+        self.assertEqual(stack._min_val, float("inf"))
+        self.assertEqual(stack._values, [])
 
 
 if __name__ == "__main__":
