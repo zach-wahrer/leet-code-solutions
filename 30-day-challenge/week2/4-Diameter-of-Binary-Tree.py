6c42b1cdf50742a43832a60a7a3f03534f006e78
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-11 10:39:47 -0600

    Add O(n) solution for 4-Diameter-of-Binary-Tree.py

--- 30-day-challenge/week2/4-Diameter-of-Binary-Tree.py
+++ 30-day-challenge/week2/4-Diameter-of-Binary-Tree.py
@@ -2,17 +2,59 @@ import unittest
 from binaryTree import TreeNode
 
 
+# Recursive solution
 def diameter_binary_tree(root: TreeNode) -> int:
-    pass
+
+    def _tree_diameter(node):
+        if not node:
+            return [0, 0]
+
+        left_diameter, longest_left = _tree_diameter(node.left)
+        right_diameter, longest_right = _tree_diameter(node.right)
+        current_diameter = longest_left + longest_right + 1
+
+        return [max(left_diameter, right_diameter, current_diameter),
+                max(longest_left, longest_right) + 1]
+
+    return max(_tree_diameter(root)) - 1 if root else 0
 
 
 class TestBinaryTreeDiameter(unittest.TestCase):
+    def test_tree1432(self):
+        root = TreeNode()
+        root.build_from_heap([1, 4, 3, 2])
+        self.assertEqual(diameter_binary_tree(root), 3)
+
     def test_tree3(self):
         root = TreeNode()
         root.build_from_heap([1, 2, 3, 4, 5])
         self.assertEqual(diameter_binary_tree(root), 3)
 
+    def test_tree_long_left(self):
+        root = TreeNode()
+        root.build_from_heap([2, 3, None, 1])
+        self.assertEqual(diameter_binary_tree(root), 2)
+
     def test_tree1(self):
         root = TreeNode()
         root.build_from_heap([1, 2])
         self.assertEqual(diameter_binary_tree(root), 1)
+
+    def test_tree2(self):
+        root = TreeNode()
+        root.build_from_heap([1, 2, 3])
+        self.assertEqual(diameter_binary_tree(root), 2)
+
+    def test_tree_empty(self):
+        root = TreeNode()
+        root.build_from_heap([])
+        self.assertEqual(diameter_binary_tree(root), 0)
+
+    def test_tree_root_only(self):
+        root = TreeNode()
+        root.build_from_heap([1])
+        self.assertEqual(diameter_binary_tree(root), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
