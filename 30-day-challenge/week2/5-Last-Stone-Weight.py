91168688c540d85488552698145739b1912b323b
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-12 08:21:57 -0600

    Add O(n**2) time solution for 5-Last-Stone-Weight.py

--- 30-day-challenge/week2/5-Last-Stone-Weight.py
+++ 30-day-challenge/week2/5-Last-Stone-Weight.py
@@ -1,8 +1,24 @@
 import unittest
 
 
+# O(n**2) time solution
 def smash_stones(stones: list) -> int:
-    pass
+    def _pop_heaviest(stones):
+        heaviest = max(stones)
+        stones.remove(heaviest)
+        return heaviest
+
+    while len(stones) >= 1:
+        if len(stones) == 1:
+            return stones[0]
+
+        heaviest = _pop_heaviest(stones)
+        sec_heaviest = _pop_heaviest(stones)
+
+        if heaviest != sec_heaviest:
+            stones.append(heaviest - sec_heaviest)
+
+    return 0
 
 
 class TestStones(unittest.TestCase):
@@ -13,7 +29,10 @@ class TestStones(unittest.TestCase):
         self.assertEqual(smash_stones([2, 2]), 0)
 
     def test_222(self):
-        self.assertEqual(smash_stones([2, 2, 2]), 1)
+        self.assertEqual(smash_stones([2, 2, 2]), 2)
+
+    def test_10(self):
+        self.assertEqual(smash_stones([10]), 10)
 
 
 if __name__ == "__main__":
