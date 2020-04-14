0a8e138c38ce0a5cd89646da3ea6b8d65c416921
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-14 11:12:44 -0600

    Improve clarity for O(n) slices solution for 7-String-Shifts.py

--- 30-day-challenge/week2/7-String-Shifts.py
+++ 30-day-challenge/week2/7-String-Shifts.py
@@ -3,7 +3,7 @@ import unittest
 
 # O(n) solution using slices
 def shift_string(s: str, shift: list) -> str:
-    def _get_final_shift(shifts: list, s_len):
+    def _get_final_shift(shifts: list):
         amount = 0
         for shift in shifts:
             if shift[0] == 1:
@@ -11,16 +11,38 @@ def shift_string(s: str, shift: list) -> str:
             else:
                 amount -= shift[1]
 
-        return (1, amount % s_len) if amount > 0 else (0, abs(amount) % s_len)
+        return (1, amount % len(s)) if amount > 0 else (0, abs(amount) % len(s))
 
     def _shift_string(shift_instruction, string):
-
+        print(shift_instruction)
         if shift_instruction[0] == 1:
             return string[-shift_instruction[1]:] + string[:-shift_instruction[1]]
         else:
             return string[shift_instruction[1]:] + string[:shift_instruction[1]]
 
-    return _shift_string(_get_final_shift(shift, len(s)), s)
+    return _shift_string(_get_final_shift(shift), s)
+
+
+# O(n) solution using slices - Improved
+def shift_string(s: str, shift: list) -> str:
+    def _get_final_shift(shifts: list):
+        amount = 0
+        for shift in shifts:
+            if shift[0] == 1:
+                amount += shift[1]
+            else:
+                amount -= shift[1]
+
+        return amount
+
+    def _shift_string(amount, string):
+        move = abs(amount) % len(s)
+        if amount > 0:
+            return string[-move:] + string[:-move]
+        else:
+            return string[move:] + string[:move]
+
+    return _shift_string(_get_final_shift(shift), s)
 
 
 class TestStringShift(unittest.TestCase):
