7a392767d888d28e57bed162e022ca053ec9ec59
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-15 07:54:52 -0600

    Add O(n**2) time and O(n) solutions for 1-Product-of-Array-Except-Self.py

--- 30-day-challenge/week3/1-Product-of-Array-Except-Self.py
+++ 30-day-challenge/week3/1-Product-of-Array-Except-Self.py
@@ -1,8 +1,37 @@
 import unittest
 
 
+# O(n**2) time solution
+def product_except_self_slow(nums: list) -> list:
+    output = []
+    for _ in range(len(nums)):
+        output.append(1)
+    for index, number in enumerate(nums):
+        for pos, num in enumerate(output):
+            if index != pos:
+                output[pos] *= number
+    return output
+
+
+# O(n) solution
 def product_except_self(nums: list) -> list:
-    pass
+    answers, left, right = [], [1], [1]
+    product = 1
+    for i in nums[:len(nums) - 1]:
+        left.append(i * product)
+        product *= i
+
+    product = 1
+    for i in nums[::-1]:
+        right.append(i * product)
+        product *= i
+    right.pop()
+    right.reverse()
+
+    for l, r in zip(right, left):
+        answers.append(r * l)
+
+    return answers
 
 
 class TestProducts(unittest.TestCase):
@@ -11,3 +40,7 @@ class TestProducts(unittest.TestCase):
 
     def test_12(self):
         self.assertEqual(product_except_self([1, 2]), [2, 1])
+
+
+if __name__ == "__main__":
+    unittest.main()
