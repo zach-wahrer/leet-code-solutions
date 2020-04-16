38536ee0460b127e1c3b03d6c14f245d4855fc2b
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-16 08:55:25 -0600

    Add in progress solution for 2-Valid-Parenthesis-String.py

--- 30-day-challenge/week3/2-Valid-Parenthesis-String.py
+++ 30-day-challenge/week3/2-Valid-Parenthesis-String.py
@@ -2,10 +2,49 @@ import unittest
 
 
 def is_valid(s: str) -> bool:
-    pass
+
+    def _scan_string(s: str, direction: str) -> list:
+        stack = []
+
+        for char in [char for char in s][::direction]:
+            if char == "(":
+                if ")" in stack:
+                    stack.remove(")")
+                elif "*" in stack:
+                    stack.remove("*")
+                else:
+                    stack.append(char)
+            elif char == ")":
+                if "(" in stack:
+                    stack.remove("(")
+                elif "*" in stack:
+                    stack.remove("*")
+                else:
+                    stack.append(")")
+            else:
+                if "(" in stack and len(stack) > 1:
+                    stack.remove("(")
+                else:
+                    stack.append(char)
+
+        if "*" in stack:
+            stack.remove("*")
+
+        return stack
+
+    if not _scan_string(s, 1) or not _scan_string(s, -1):
+        return True
+
+    return False
 
 
 class TestValidParenthesis(unittest.TestCase):
+    def test_unmatched(self):
+        self.assertFalse(is_valid(")("))
+
+    def test_wild_as_right(self):
+        self.assertTrue(is_valid("(*()"))
+
     def test_simple(self):
         self.assertTrue(is_valid("()"))
 
