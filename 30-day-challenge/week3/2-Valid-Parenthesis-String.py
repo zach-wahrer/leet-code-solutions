c0084e8e32506e3d0b187b626117a01c3af27c2f
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-16 11:50:11 -0600

    Add O(n) time/space solution for 2-Valid-Parenthesis-String.py

--- 30-day-challenge/week3/2-Valid-Parenthesis-String.py
+++ 30-day-challenge/week3/2-Valid-Parenthesis-String.py
@@ -1,44 +1,40 @@
 import unittest
 
 
+# O(n) time/space solution
 def is_valid(s: str) -> bool:
 
-    def _scan_string(s: str, direction: str) -> list:
-        stack = []
-
-        for char in [char for char in s][::direction]:
-            if char == "(":
-                if ")" in stack:
-                    stack.remove(")")
-                elif "*" in stack:
-                    stack.remove("*")
-                else:
-                    stack.append(char)
-            elif char == ")":
-                if "(" in stack:
-                    stack.remove("(")
-                elif "*" in stack:
-                    stack.remove("*")
-                else:
-                    stack.append(")")
+    stack = []
+    for char in s:
+        if char == '(' or char == '*':
+            stack.append(char)
+        else:
+            if len(stack) > 0:
+                stack.pop()
             else:
-                if "(" in stack and len(stack) > 1:
-                    stack.remove("(")
-                else:
-                    stack.append(char)
+                return False
 
-        if "*" in stack:
-            stack.remove("*")
+    stack = []
+    for char in s[::-1]:
+        if char == ')' or char == '*':
+            stack.append(char)
+        else:
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                return False
 
-        return stack
-
-    if not _scan_string(s, 1) or not _scan_string(s, -1):
-        return True
-
-    return False
+    return True
 
 
 class TestValidParenthesis(unittest.TestCase):
+    def test_complex(self):
+        self.assertTrue(is_valid(
+            "((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()"))
+
+    def test_wildcards(self):
+        self.assertTrue(is_valid("(((******)))"))
+
     def test_unmatched(self):
         self.assertFalse(is_valid(")("))
 
