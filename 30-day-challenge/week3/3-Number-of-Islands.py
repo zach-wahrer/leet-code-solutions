7f3fd118ec21628fc5142340a6b09d00652cdf85
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-17 09:13:41 -0600

    Add O(n*m) solution for 3-Number-of-Islands.py

--- 30-day-challenge/week3/3-Number-of-Islands.py
+++ 30-day-challenge/week3/3-Number-of-Islands.py
@@ -2,7 +2,48 @@ import unittest
 
 
 def num_of_islands(grid: list) -> int:
-    pass
+
+    def _key_gen(row, col, memoize, grid):
+        if row not in range(len(grid)) or col not in range(len(grid[0])):
+            return "oob"
+
+        key = (row, col)
+        if key in memoize:
+            return "seen"
+        memoize.add(key)
+        return key
+
+    def _spread(key, memoize, grid):
+        if key == "seen" or key == "oob":
+            return
+        row, col = key
+        if grid[row][col] == 0:
+            return
+        right = _key_gen(row, col + 1, memoize, grid)
+        left = _key_gen(row, col - 1, memoize, grid)
+        up = _key_gen(row - 1, col, memoize, grid)
+        down = _key_gen(row + 1, col, memoize, grid)
+
+        if right != 'seen' or 'oob':
+            _spread(right, memoize, grid)
+        if left != 'seen' or 'oob':
+            _spread(left, memoize, grid)
+        if up != 'seen' or 'oob':
+            _spread(up, memoize, grid)
+        if down != 'seen' or 'oob':
+            _spread(down, memoize, grid)
+
+    memoize = set()
+    count = 0
+
+    for row_index, row in enumerate(grid):
+        for col_index, col in enumerate(row):
+            key = _key_gen(row_index, col_index, memoize, grid)
+            if key != 'seen' and col == 1:
+                count += 1
+                _spread(key, memoize, grid)
+
+    return count
 
 
 class TestIslands(unittest.TestCase):
@@ -10,7 +51,6 @@ class TestIslands(unittest.TestCase):
         map = [
             [1, 1, 1, 1, 0],
             [1, 1, 0, 1, 0],
-            [1, 1, 0, 0, 0],
             [1, 1, 0, 0, 0],
             [0, 0, 0, 0, 0]
         ]
@@ -20,7 +60,6 @@ class TestIslands(unittest.TestCase):
         map = [
             [1, 1, 0, 0, 0],
             [1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0],
             [0, 0, 1, 0, 0],
             [0, 0, 0, 1, 1]
         ]
