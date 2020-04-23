ff5870e4d55a8db8e4ff6b3dafa89b1efdae538f
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-23 07:45:17 -0600

    Add shift solution for 2-Bitwise-AND-of-Num-Range.py

--- 30-day-challenge/week4/2-Bitwise-AND-of-Num-Range.py
+++ 30-day-challenge/week4/2-Bitwise-AND-of-Num-Range.py
@@ -2,7 +2,12 @@ import unittest
 
 
 def get_and_range(m: int, n: int) -> int:
-    pass
+    i = 0
+    while m != n:
+        m >>= 1
+        n >>= 1
+        i += 1
+    return n << i
 
 
 class TestBitwiseANDRange(unittest.TestCase):
