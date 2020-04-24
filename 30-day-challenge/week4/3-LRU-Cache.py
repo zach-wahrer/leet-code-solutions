c73e79279a9fabb7fa4ed7e292c22f2ee2561f32
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-24 09:12:41 -0600

    Add tests for 3-LRU-Cache.py

---
+++ 30-day-challenge/week4/3-LRU-Cache.py
@@ -0,0 +1,31 @@
+import unittest
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        pass
+
+    def get(self, key: int) -> int:
+        pass
+
+    def put(self, key: int, value: int) -> None:
+        pass
+
+
+class TestLRUCache(unittest.TestCase):
+    def test_operations(self):
+        cache = LRUCache(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(3, 3)
+        self.assertEqual(cache.get(2), -1)
+        cache.put(4, 4)
+        self.assertEqual(cache.get(1), -1)
+        self.assertEqual(cache.get(3), 3)
+        self.assertEqual(cache.get(4), 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
