a757e445e59551518737f9099e613db139cc18f9
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-24 09:49:33 -0600

    Add initial O(n) solution for 3-LRU-Cache.py

--- 30-day-challenge/week4/3-LRU-Cache.py
+++ 30-day-challenge/week4/3-LRU-Cache.py
@@ -1,16 +1,34 @@
 import unittest
+from collections import deque
 
 
+# Initial solution - O(n) because of _refresh_value's remove()
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass
+        self.LRU_queue = deque(["" for i in range(capacity)])
+        self.value_store = {}
 
     def get(self, key: int) -> int:
-        pass
+        if key in self.value_store:
+            self._refresh_value(key)
+            return self.value_store[key]
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        pass
+        if key in self.value_store:
+            self._refresh_value(key)
+        else:
+            oldest = self.LRU_queue.popleft()
+            if oldest in self.value_store:
+                self.value_store.pop(oldest)
+            self.LRU_queue.append(key)
+
+        self.value_store[key] = value
+
+    def _refresh_value(self, key: int) -> None:
+        self.LRU_queue.remove(key)
+        self.LRU_queue.append(key)
 
 
 class TestLRUCache(unittest.TestCase):
