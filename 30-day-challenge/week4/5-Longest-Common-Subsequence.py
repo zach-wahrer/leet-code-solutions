38caed073280cef8cce5991a944e24e876da81d4
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-26 11:01:27 -0600

    Add dynamic O(n*m) solution for 5-Longest-Common-Subsequence.py

--- 30-day-challenge/week4/5-Longest-Common-Subsequence.py
+++ 30-day-challenge/week4/5-Longest-Common-Subsequence.py
@@ -1,8 +1,21 @@
 import unittest
 
 
+# Dynamic O(n*m) solution
 def longest_common_subseq(text1: str, text2: str) -> int:
-    pass
+    len1 = len(text1)
+    len2 = len(text2)
+
+    memoize = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+
+    for i in range(len1 - 1, -1, -1):
+        for j in range(len2 - 1, -1, -1):
+            if text1[i] == text2[j]:
+                memoize[i][j] = memoize[i + 1][j + 1] + 1
+            else:
+                memoize[i][j] = max(memoize[i + 1][j], memoize[i][j + 1], memoize[i + 1][j + 1])
+
+    return memoize[0][0]
 
 
 class TestLongestCommonSubsequence(unittest.TestCase):
