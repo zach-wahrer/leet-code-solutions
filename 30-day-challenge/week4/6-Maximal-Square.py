02c06d6ee19010faacfb3dbc5e7126583e378ef3
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-27 15:09:36 -0600

    Add partial solution for 6-Maximal-Square.py

--- 30-day-challenge/week4/6-Maximal-Square.py
+++ 30-day-challenge/week4/6-Maximal-Square.py
@@ -1,16 +1,67 @@
 import unittest
 
 
+# Doesn't pass all leetcode tests
 def max_square(matrix: list) -> int:
-    pass
+
+    def _key_gen(row, col):
+        if row not in range(len(matrix)) or col not in range(len(matrix[0])):
+            return False
+        key = (row, col)
+        return key
+
+    def _measure_col(row, col):
+        square_col_size = 0
+        while _key_gen(row, col) and matrix[row][col] == "1":
+            square_col_size += 1
+            col += 1
+        return square_col_size
+
+    def _measure_square(row, col):
+        square_row_size = 0
+        column_sizes = []
+        while _key_gen(row, col) and matrix[row][col] == "1":
+            column_sizes.append(_measure_col(row, col))
+            square_row_size += 1
+            row += 1
+        return min(column_sizes + [square_row_size])
+
+    square_dimensions = []
+
+    for row_index, row_value in enumerate(matrix):
+        for col_index, col_value in enumerate(row_value):
+            if col_value == "1":
+                square_dimensions.append(_measure_square(row_index, col_index))
+
+    return max(square_dimensions) ** 2 if square_dimensions else 0
 
 
 class TestMaxSquare(unittest.TestCase):
     def test_4(self):
         grid = [
-            ["1 0 1 0 0"],
-            ["1 0 1 1 1"],
-            ["1 1 1 1 1"],
-            ["1 0 0 1 0"]
+            ["1", "0", "1", "0", "0"],
+            ["1", "0", "1", "1", "1"],
+            ["1", "1", "1", "1", "1"],
+            ["1", "0", "0", "1", "0"]
         ]
         self.assertEqual(max_square(grid), 4)
+
+    def test_1(self):
+        grid = [["1"]]
+        self.assertEqual(max_square(grid), 1)
+
+    def test_9(self):
+        grid = [
+            ["0", "0", "1", "0"],
+            ["1", "1", "1", "1"],
+            ["1", "1", "1", "1"],
+            ["1", "1", "1", "0"],
+            ["1", "1", "0", "0"],
+            ["1", "1", "1", "1"],
+            ["1", "1", "1", "0"]
+        ]
+        self.assertEqual(max_square(grid), 9)
+
+
+if __name__ == "__main__":
+    unittest.main()
