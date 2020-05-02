a99b889cbfdc4bd6766ff11491f204a42c21d440
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-02 08:14:09 -0600

    Add O(n**2) time solution for 1299-Replace-Elements-with-Greatest-Element.py

--- easy/1299-Replace-Elements-with-Greatest-Element.py
+++ easy/1299-Replace-Elements-with-Greatest-Element.py
@@ -1,8 +1,15 @@
 import unittest
 
 
+# O(n**2) time solution
 def replace_elements(arr: list) -> list:
-    pass
+    for index in range(len(arr)):
+        if index == len(arr) - 1:
+            arr[index] = -1
+        else:
+            arr[index] = max(arr[index + 1:])
+
+    return arr
 
 
 class TestReplaceElements(unittest.TestCase):
@@ -10,3 +17,17 @@ class TestReplaceElements(unittest.TestCase):
         arr = [17, 18, 5, 4, 6, 1]
         output = [18, 6, 6, 6, 1, -1]
         self.assertEqual(replace_elements(arr), output)
+
+    def test_easy(self):
+        arr = [1, 2, 3, 4]
+        output = [4, 4, 4, -1]
+        self.assertEqual(replace_elements(arr), output)
+
+    def test_one(self):
+        arr = [1]
+        output = [-1]
+        self.assertEqual(replace_elements(arr), output)
+
+
+if __name__ == "__main__":
+    unittest.main()
