9e13e90423626d363eaa32f3ba6690715f40c816
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-03 09:09:35 -0600

    Add O(1) time/space solution for 191-Number-of-1-Bits.py

--- easy/191-Number-of-1-Bits.py
+++ easy/191-Number-of-1-Bits.py
@@ -1,8 +1,14 @@
 import unittest
 
 
+# O(1) space and time solution
 def num_1_bits(n: int) -> int:
-    pass
+    one_count = 0
+    for i in list(bin(n))[2:]:
+        if i == "1":
+            one_count += 1
+
+    return one_count
 
 
 class TestNum1Bits(unittest.TestCase):
@@ -14,3 +20,7 @@ class TestNum1Bits(unittest.TestCase):
 
     def test_4294967293(self):
         self.assertEqual(num_1_bits(4294967293), 31)
+
+
+if __name__ == "__main__":
+    unittest.main()
