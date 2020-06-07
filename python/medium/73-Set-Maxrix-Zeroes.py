935e32598d6b163269e9354b3b397674425de81e
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-07 13:49:36 -0600

    Add O(n*m) time, O(1) space solution for 73-Set-Maxrix-Zeroes.py

--- python/medium/73-Set-Maxrix-Zeroes.py
+++ python/medium/73-Set-Maxrix-Zeroes.py
@@ -1,16 +1,88 @@
 import unittest
 
-def process_matrix(maxrix: list) -> None:
-    pass
 
-def find_zeroes(matrix: list) -> list:
-    pass
+# O(n*m) time, O(1) space solution
+class Solution:
+    def __init__(self):
+        self._matrix = []
+        self._locations = []
 
-def set_zeroes(locations: list) -> None:
-    pass
+    def get_matrix(self):
+        return self._matrix
 
+    def setZeroes(self, matrix: list) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self._matrix = matrix
+        self.process_matrix()
+
+    def process_matrix(self) -> None:
+        self._find_zeroes()
+        if self._locations:
+            self._set_zeroes()
+
+    def _find_zeroes(self):
+        for row_index, row in enumerate(self._matrix):
+            for col_index, col in enumerate(row):
+                if col == 0:
+                    self._locations.append((row_index, col_index))
+
+    def _set_zeroes(self) -> None:
+        for location in self._locations:
+            self._spread_zeroes(location[0], location[1])
+
+    def _spread_zeroes(self, row, col):
+        self._spread_zeroes_left(row, col - 1)
+        self._spread_zeroes_right(row, col + 1)
+        self._spread_zeroes_down(row + 1, col)
+        self._spread_zeroes_up(row - 1, col)
+
+    def _inbounds(self, row, col):
+        if row in range(len(self._matrix)) and col in range(len(self._matrix[0])):
+            return True
+        return False
+
+    def _spread_zeroes_left(self, row, col):
+        if not self._inbounds(row, col):
+            return
+        self._matrix[row][col] = 0
+        self._spread_zeroes_left(row, col - 1)
+
+    def _spread_zeroes_right(self, row, col):
+        if not self._inbounds(row, col):
+            return
+        self._matrix[row][col] = 0
+        self._spread_zeroes_right(row, col + 1)
+
+    def _spread_zeroes_down(self, row, col):
+        if not self._inbounds(row, col):
+            return
+        self._matrix[row][col] = 0
+        self._spread_zeroes_down(row + 1, col)
+
+    def _spread_zeroes_up(self, row, col):
+        if not self._inbounds(row, col):
+            return
+        self._matrix[row][col] = 0
+        self._spread_zeroes_up(row - 1, col)
 
 class TestSetZeroes(unittest.TestCase):
+    def test_non_square(self):
+        matrix = [
+            [0, 1, 2, 0],
+            [3, 4, 5, 2],
+            [1, 3, 1, 5]
+        ]
+        solution = [
+            [0, 0, 0, 0],
+            [0, 4, 5, 0],
+            [0, 3, 1, 0]
+        ]
+        solve = Solution()
+        solve.setZeroes(matrix)
+        self.assertEqual(solve.get_matrix(), solution)
+
     def test_middle_zero(self):
         matrix = [
             [1, 1, 1],
@@ -22,7 +94,9 @@ class TestSetZeroes(unittest.TestCase):
             [0, 0, 0],
             [1, 0, 1]
         ]
-        self.assertEqual(process_matrix(matrix), solution)
+        solve = Solution()
+        solve.setZeroes(matrix)
+        self.assertEqual(solve.get_matrix(), solution)
 
     def test_corner_zeroes(self):
         matrix = [
@@ -35,7 +109,9 @@ class TestSetZeroes(unittest.TestCase):
             [0, 1, 0],
             [0, 0, 0]
         ]
-        self.assertEqual(process_matrix(matrix), solution)
+        solve = Solution()
+        solve.setZeroes(matrix)
+        self.assertEqual(solve.get_matrix(), solution)
 
     def test_all_zeroes(self):
         matrix = [
@@ -48,7 +124,9 @@ class TestSetZeroes(unittest.TestCase):
             [0, 0, 0],
             [0, 0, 0]
         ]
-        self.assertEqual(process_matrix(matrix), solution)
+        solve = Solution()
+        solve.setZeroes(matrix)
+        self.assertEqual(solve.get_matrix(), solution)
 
     def test_no_zeroes(self):
         matrix = [
@@ -61,4 +139,10 @@ class TestSetZeroes(unittest.TestCase):
             [1, 1, 1],
             [1, 1, 1]
         ]
-        self.assertEqual(process_matrix(matrix), solution)
+        solve = Solution()
+        solve.setZeroes(matrix)
+        self.assertEqual(solve.get_matrix(), solution)
+
+
+if __name__ == "__main__":
+    unittest.main()
