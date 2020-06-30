802849f85dede6ed1dc8baf4b1aadb962ce951d5
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-30 16:50:09 -0600

    Add solution for 1-Two-Sum.rb

---
+++ ruby/easy/1-Two-Sum.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+
+def two_sums(nums, target)
+  compliments = {}
+  nums.each_with_index do |num, index|
+    return [compliments[num], index] if compliments.has_key?(num)
+    compliments[target - num] = index
+  end
+end
+
+puts two_sums([2, 7, 11, 15], 9) == [0, 1]
+puts two_sums([2, 7, 11, 15, 1, 13], 14) == [4, 5]
