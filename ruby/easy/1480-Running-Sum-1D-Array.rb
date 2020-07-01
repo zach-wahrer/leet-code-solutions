44ed66a550f82716c0957ea1497ca64aaa0bafe6
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-01 14:01:15 -0600

    Add O(n) time, O(1) space solution for 1480-Running-Sum-1D-Array.rb

--- ruby/easy/1480-Running-Sum-1D-Array.rb
+++ ruby/easy/1480-Running-Sum-1D-Array.rb
@@ -1,4 +1,13 @@
 def running_sum(nums)
+  index = 1
+  while index < nums.length
+    nums[index] = nums[index] + nums[index - 1]
+    index += 1
+  end
+  nums
+end
+
+def running_sum_extra_memory(nums)
   result = [nums[0]]
   index = 1
   while index < nums.length
