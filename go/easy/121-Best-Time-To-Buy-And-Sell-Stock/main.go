64f1d44ff7b60f28e80bebdfc366159ed1061ab4
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-03 09:04:52 -0600

    Revise solution to use ints instead of -inf

--- go/easy/121-Best-Time-To-Buy-And-Sell-Stock/main.go
+++ go/easy/121-Best-Time-To-Buy-And-Sell-Stock/main.go
@@ -9,21 +9,21 @@ func main() {
 }
 
 func getMaxProfit(prices []int) int {
-	max_price := math.Inf(-1)
-	min_price := math.Inf(1)
-	max_profit := 0.0
+	max_price := -1
+	min_price := -1
+	max_profit := 0
 
 	for _, num := range prices {
-		if float64(num) > max_price && min_price != math.Inf(-1) {
-			max_price = float64(num)
-			max_profit = math.Max(max_profit, max_price-min_price)
+		if num > max_price && min_price != -1 {
+			max_price = num
+			max_profit = int(math.Max(float64(max_profit), float64(max_price-min_price)))
 		}
 
-		if float64(num) < min_price || min_price == math.Inf(-1) {
-			min_price = float64(num)
-			max_price = math.Inf(-1)
+		if num < min_price || min_price == -1 {
+			min_price = num
+			max_price = -1
 		}
 	}
 
-	return int(max_profit)
+	return max_profit
 }
